e8ab1a031915d7c6442ca395fc3149b0a6ed894e
cracked-machine <chrisjsutton@gmail.com>
2025-08-17 10:52:12 +0000

    comments

--- inc/components/random.hpp
+++ inc/components/random.hpp
@@ -8,16 +8,23 @@ namespace ProceduralMaze::Cmp {
 
 class Random : public Cmp::Base {
 public:
+    // set the rng range
     Random(int min, int max) : m_intdist(min, max) { SPDLOG_DEBUG("Random()"); }
     ~Random() { SPDLOG_DEBUG("~Random()"); }
+
+    // get the next random number
     int gen() { return m_intdist( m_randgen ); }
 
+    // set the seed for random generator
     static void seed(unsigned long s) 
     {
         m_seed = s;
         m_randgen = std::mt19937(s);
     }
+
+    // get the seed for random generator
     static unsigned long seed() { return m_seed; }
+
 private:
     static unsigned long m_seed;
     static inline std::mt19937 m_randgen{std::random_device{}()};
