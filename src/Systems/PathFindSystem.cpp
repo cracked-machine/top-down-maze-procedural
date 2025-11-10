3f19abf27a581cac895288be15c126cef72c4624
cracked-machine <chrisjsutton@gmail.com>
2025-11-10 22:58:02 +0000

    tidied up scanForPlayers()

--- src/Systems/PathFindSystem.cpp
+++ src/Systems/PathFindSystem.cpp
@@ -86,30 +86,29 @@ void PathFindSystem::scanForPlayers( entt::entity npc_entity, entt::entity playe
 
     auto nearest_obstacle = distance_queue.top();
 
-    // now lets consider moving our NPC. We use lerp to get a smooth transition from grid movements.
+    // Set the Direction vector and LerpPosition target Coords for next movement towards the player
     auto npc_cmp = m_reg->try_get<Cmp::NPC>( npc_entity );
-    if ( npc_cmp )
-    {
-      // do not interrupt mid lerp
-      auto npc_lerp_pos_cmp = m_reg->try_get<Cmp::LerpPosition>( npc_entity );
-      if ( npc_lerp_pos_cmp && npc_lerp_pos_cmp->m_lerp_factor < 1.0f ) {}
-      else
-      {
-        auto npc_pos = m_reg->try_get<Cmp::Position>( npc_entity );
-        auto player_pos = m_reg->try_get<Cmp::Position>( player_entity );
-        if ( !npc_pos || !player_pos ) return;
-        auto distance = ( *player_pos ).position - ( *npc_pos ).position;
-        if ( distance != sf::Vector2f( 0.0f, 0.0f ) )
-        {
-          m_reg->emplace_or_replace<Cmp::Direction>( npc_entity, distance.normalized() );
-        }
-        // Otherwise set target position for lerp. This will get updated in the main engine loop via LerpSystems
-        auto move_candidate_pixel_pos = getPixelPosition( nearest_obstacle.second );
-        if ( not move_candidate_pixel_pos ) return;
-        auto npc_lerp_speed = get_persistent_component<Cmp::Persistent::NpcLerpSpeed>();
-        m_reg->emplace_or_replace<Cmp::LerpPosition>( npc_entity, move_candidate_pixel_pos.value(), npc_lerp_speed.get_value() );
-      }
-    }
+    if ( not npc_cmp ) return;
+
+    auto npc_lerp_pos_cmp = m_reg->try_get<Cmp::LerpPosition>( npc_entity );
+    if ( npc_lerp_pos_cmp && npc_lerp_pos_cmp->m_lerp_factor < 1.0f ) return;
+
+    auto npc_pos = m_reg->try_get<Cmp::Position>( npc_entity );
+    auto player_pos = m_reg->try_get<Cmp::Position>( player_entity );
+    if ( !npc_pos || !player_pos ) return;
+
+    auto distance = ( *player_pos ).position - ( *npc_pos ).position;
+    SPDLOG_INFO( " NPC entity {} moving towards player from obstacle at distance {},{}", static_cast<int>( npc_entity ), distance.x,
+                 distance.y );
+    if ( distance == sf::Vector2f( 0.0f, 0.0f ) ) return;
+
+    m_reg->emplace_or_replace<Cmp::Direction>( npc_entity, distance.normalized() );
+
+    auto move_candidate_pixel_pos = getPixelPosition( nearest_obstacle.second );
+    if ( not move_candidate_pixel_pos ) return;
+
+    auto npc_lerp_speed = get_persistent_component<Cmp::Persistent::NpcLerpSpeed>();
+    m_reg->emplace_or_replace<Cmp::LerpPosition>( npc_entity, move_candidate_pixel_pos.value(), npc_lerp_speed.get_value() );
   }
 }
 
