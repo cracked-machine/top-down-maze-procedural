7269dd344b425cd2e7625392169f790997c9536c
cracked-machine <chrisjsutton@gmail.com>
2026-04-14 21:57:35 +0000

    fixced lava sfx after death bug

--- src/SceneControl/Scenes/GameOverScene.cpp
+++ src/SceneControl/Scenes/GameOverScene.cpp
@@ -17,6 +17,7 @@ void GameOverScene::on_enter()
   auto &persistent_sys = static_cast<Sys::PersistSystem &>( m_sys.find<Sys::Store::Type::PersistSystem>() );
   persistent_sys.initializeComponentRegistry();
   persistent_sys.load_state();
+  m_sound_bank.get_effect( "bubbling_lava" ).stop();
 }
 void GameOverScene::on_exit()
 {
